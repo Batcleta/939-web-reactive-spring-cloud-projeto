ca2284698f1c261cb0e0c07b5905128f1d39edd2
Washington Ferreira <35137108+Batcleta@users.noreply.github.com>
2023-04-24 16:34:15 -0300

    user refactor

--- spring-webflux/src/main/java/tech/ada/user/controller/UserController.java
+++ spring-webflux/src/main/java/tech/ada/user/controller/UserController.java
@@ -28,53 +28,50 @@ public class UserController {
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<ResponseEntity<User>> salvar(@RequestBody User user) {
         return service.salvar(user)
-            .map(atual -> ResponseEntity.ok().body(atual));
+                .map(atual -> ResponseEntity.ok().body(atual));
     }
 
     @GetMapping // TODO nao retorna noContent quando lista vazia
     public Mono<ResponseEntity<Flux<User>>> listar() {
         return service.listar()
-            .collectList()
-            .map(users -> ResponseEntity.ok().body(Flux.fromIterable(users)) )
-            .switchIfEmpty(Mono.just(ResponseEntity.noContent().build()));
+                .collectList()
+                .map(users -> ResponseEntity.ok().body(Flux.fromIterable(users)) )
+                .switchIfEmpty(Mono.just(ResponseEntity.noContent().build()));
     }
 
     @GetMapping("/{id}")
     public Mono<ResponseEntity<User>> getById(@PathVariable String id) {
         return service.buscarPorId(id)
-            .map(atual -> ResponseEntity.ok().body(atual))
-            .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
+                .map(atual -> ResponseEntity.ok().body(atual))
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
     }
 
     @GetMapping("/usernames")
     public Mono<ResponseEntity<Flux<User>>> buscarPorUsername(@RequestParam("users") String[] users) {
         return service.buscarPorUsernames(users)
-            .collectList()
-            .map(atual -> ResponseEntity.ok().body(Flux.fromIterable(atual)))
-            .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
+                .collectList()
+                .map(atual -> ResponseEntity.ok().body(Flux.fromIterable(atual)))
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
     }
 
     @PutMapping("/{id}")
     public Mono<ResponseEntity<User>> atualizar(@RequestBody UserRequest user, @PathVariable String id) {
         return service.atualizar(user.create(), id)
-            .map(atual -> ResponseEntity.ok().body(atual))
-            .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
+                .map(atual -> ResponseEntity.ok().body(atual))
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
     }
 
-//    @PostMapping(value = "/pagamentos", produces = MediaType.APPLICATION_JSON_VALUE)
-//    public Mono<Comprovante> pagamento(@RequestBody Comprovante comprovante) {
-//        comprovante.setAck_usuario(true);
-//        /// pegar os objetos, mudar os dados e salvar
-//        // pegar o valor e diminuir de pagador e aumentar do recebedor e salvar
-//        return Mono.just(comprovante).log();
-//    }
+    @PostMapping(value = "/pagamentos", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<Comprovante> pagamento(@RequestBody Comprovante comprovante) {
+        return service.atualizar(comprovante);
+    }
 
     @DeleteMapping("/{id}")
     public Mono<ResponseEntity<Void>> remover(@PathVariable String id) {
         return service.remover(id)
-            .then(Mono.just(ResponseEntity.ok().<Void>build()))
-            .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()))
-            .onErrorResume(e -> Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()));
+                .then(Mono.just(ResponseEntity.ok().<Void>build()))
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()))
+                .onErrorResume(e -> Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()));
     }
 
 }
